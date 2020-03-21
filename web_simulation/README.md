4b85d8891cd23d34da7ef642328f3354d5660127
DerWaldi <murgul.sebastian@outlook.com>
2020-03-21 23:37:01 +0100

    basic layout for web simulation

--- web_simulation/README.md
+++ web_simulation/README.md
@@ -66,3 +66,9 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+## Notes
+
+### plotly out of memory
+
+react-scripts --max_old_space_size=8192 build
--- web_simulation/src/App.js
+++ web_simulation/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
-import { withStyles, Container, Typography, Grid } from "@material-ui/core";
-import HorizontalTimeline from "react-horizontal-timeline";
-
-function createTimeStamps() {
-  var now = new Date();
-  var daysOfYear = [];
-  for (var d = new Date(2020, 1, 1); d <= now; d.setDate(d.getDate() + 1)) {
-    daysOfYear.push(new Date(d));
-  }
-
-  return daysOfYear;
-}
-
-const VALUES = createTimeStamps();
+import {
+  withStyles,
+  Container,
+  Typography,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Checkbox,
+  Slider
+} from "@material-ui/core";
+import FolderIcon from "@material-ui/icons/Folder";
+import Dashboard from "./components/Dashboard";
 
 class App extends React.Component {
   state = { value: 0, previous: 0 };
@@ -21,46 +21,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Grid
-          container
-          spacing={4}
-          style={{ background: "#fff", height: "150px" }}
-        ></Grid>
-        <Grid
-          container
-          spacing={4}
-          style={{ height: "calc(100vh - 100px - 300px)" }}
-        >
-          <Grid
-            item
-            xs={4}
-            style={{ background: "#555", height: "100%" }}
-          ></Grid>
-          <Grid item xs={8} style={{ background: "#999" }}></Grid>
-        </Grid>
-        <Grid
-          container
-          spacing={4}
-          style={{ background: "#fff", height: "300px" }}
-        >
-          <div
-            style={{
-              width: "calc(100% - 50px)",
-              height: "100px",
-              margin: "auto"
-            }}
-          >
-            <HorizontalTimeline
-              labelWidth={50}
-              linePadding={50}
-              index={this.state.value}
-              indexClick={index => {
-                this.setState({ value: index, previous: this.state.value });
-              }}
-              values={VALUES}
-            />
-          </div>
-        </Grid>
+        <Dashboard />
       </div>
     );
   }
