7a5f68237324dd2a9f3e85205dc711d0265b5fd3
Lennart Schmidt <lennart.schmidt@ufz.de>
2020-03-23 04:02:04 +0100

    First successful fit

--- api/FlatCurver/simulation/PandemicSimulator/FitPandemicSimulator.py
+++ api/FlatCurver/simulation/PandemicSimulator/FitPandemicSimulator.py
@@ -6,6 +6,8 @@ from scipy.optimize import least_squares
 
 from FlatCurver.simulation.PandemicSimulator.PandemicSimulator import PandemicSimulator
 
+####TODO
+# change y to yt becasue there already is y in deriv
 class FitPandemicSimulator(PandemicSimulator):
     def __init__(self, beta, gamma, delta, N, y, group_names,timesteps=400):
 
@@ -27,8 +29,8 @@ class FitPandemicSimulator(PandemicSimulator):
     def set_y0(self, y):
         self.y0 = y[0,:]
 
-    def _residuals(self, estbeta):
-        self.beta = self.make_time_dependent(estbeta[0], self.timesteps)#[0] so that it is automatically turned into vector
+    def _residuals(self, beta0):
+        self.beta = self.make_time_dependent(beta0[0], self.timesteps)#[0] so that it is automatically turned into vector
         output = self.simulate_extract_df().to_numpy()
         residuals = output.flatten() - self.y.flatten() #leastqsr sovler only accepts 1D-outputs
         return residuals
@@ -37,4 +39,34 @@ class FitPandemicSimulator(PandemicSimulator):
         x_0 = 5#starting value
         #bounds = [0,5]
         result = least_squares(fun=self._residuals, x0=x_0, method='lm')  #bounds=bounds
-        return(result)
+        beta_est = result.x[0]
+        self.beta = self.make_time_dependent(beta_est, self.timesteps)#[0] so that it is automatically turned into vector
+        print("Estimated beta: "+str(beta_est))
+        return(beta_est)#estimated beta
+
+    def fit_and_show_results(self):
+        beta_est = self.fit_SEIR()
+        sol = self.simulate_SEIR()
+        self.plot(sol)
+
+    def fit_and_extract_df(self):
+        beta_est = self.fit_SEIR()
+        df = self.simulate_extract_df()
+        return(df)
+
+    def plot(self, sol):
+        # TODO
+        # CALCULATE BACK INTO PERCENT  (/ self.N)
+        fig = plt.figure(facecolor='w', figsize=(20, 10))
+        ax = fig.add_subplot(111, axisbelow=True)
+        # ax.plot(self.dates[sol.t.astype(int)], sol.y[0, :], 'b', alpha=0.5, lw=2, label=f'Susceptible')
+        ax.plot(self.dates[sol.t.astype(int)], sol.y[1, :], 'r', alpha=0.5, lw=2, label=f'Dead')
+        ax.plot(self.dates[sol.t.astype(int)], sol.y[2, :], 'g', alpha=0.5, lw=2, label=f'Infected')
+        ax.plot(self.dates[sol.t.astype(int)], sol.y[3, :], 'darkgrey', alpha=0.5, lw=2,
+                label=f'Recovered with immunity')
+        #ax.plot(self.dates[sol.t.astype(int)], self.y[:, 0], 'darkblue', alpha=0.5, lw=2, label=f'Susceptible')
+        ax.plot(self.dates[sol.t.astype(int)], self.y[:, 1], 'darkred', alpha=0.5, lw=2, label=f'Dead')
+        ax.plot(self.dates[sol.t.astype(int)], self.y[:, 2], 'darkgreen', alpha=0.5, lw=2, label=f'Infected')
+        ax.plot(self.dates[sol.t.astype(int)], self.y[:, 3], 'black', alpha=0.5, lw=2,
+                label=f'Recovered with immunity')
+        self.plotting_standards(ax)
--- api/FlatCurver/simulation/PandemicSimulator/runscript.py
+++ api/FlatCurver/simulation/PandemicSimulator/runscript.py
@@ -7,8 +7,8 @@ import sys
 os.chdir("/Users/schmidle/Documents/GIT-Projects/FlatCurver/")
 #os.chdir("/Users/schmidle/Documents/GIT-Projects/FlatCurver/api/FlatCurver/simulation/PandemicSimulator")
 # automatic reloading the packages to keep tehm uodated)
-%load_ext autoreload
-%autoreload 2
+#%load_ext autoreload
+#%autoreload 2
 module_folder = os.path.abspath('/Users/schmidle/Documents/GIT-Projects/FlatCurver/api')
 if module_folder not in sys.path:
     sys.path.append(module_folder)
@@ -37,9 +37,9 @@ import FlatCurver.simulation.PandemicSimulator.FitPandemicSimulator as panfit
 # multi_pan.simulate_and_show_results()
 
 ### MyCalibration (only single input model)
-ger_pop = 8*10e7  #german population
+# ger_pop = 8*10e7  #german population
  # contact rate: given by RKI, a patient is 10 days infectious and infects 2 people during this time
-N = 8*10e7  #german population
+# N = 8*10e7  #german population
 # try if single simulator still works
 # pans = pansingle.PandemicSimulator(beta=beta, gamma=gamma, delta=delta, N=N, timesteps=timesteps,group_names=group_names)
 # pans.set_y0(y0)
@@ -47,21 +47,16 @@ N = 8*10e7  #german population
 # df = pans.simulate_extract_df()
 # df-dummy_y
 # FIT
+################
+###PREPARE DATA
+################
 
 # get data
 indata = pd.read_csv("code/01_data acquisition/dataset.csv")
 einwohner = pd.read_csv('data/einwohner_bundeslaender.csv', sep='\t')
 bland="Bayern"
 
-# initial settings
-beta = 2/10 # to be calibrated
-gamma = 0.97/14  #recovery rate: 0.97 percent survive while the average infection lasts for 14 days
-delta = 0.03/14  #death rate: 1-gamma
-timesteps=len(indata)
-y0 = [N-1,0,1,0]
-group_names = [bland]
-indata.columns
-
+# get subset
 # Use morgenpost because it contains "recovered"
 wantedcols = indata.columns.str.contains(bland+":morgenpost")#:RKI
 indata = indata.loc[:,wantedcols]
@@ -71,24 +66,40 @@ indata.columns = indata.columns.str.strip(bland+":morgenpost:")
 indata = indata.loc[indata.confirmed>0,:]
 
 # Initial values
-N = einwohner.loc[einwohner.Bundesland==bland].Einwohner
-I0 = indata.confirmed.iloc[0]
-N0 = N-I0
+N = einwohner.loc[einwohner.Bundesland==bland].Einwohner.values#n population
+I0 = indata.confirmed.iloc[0]#case number on first day of infections
+N0 = N-I0#susceptible on first day
+beta0 = 2/10 # to be calibrated
+gamma = 0.97/14  #recovery rate, fixed
+delta = 0.03/14  #death rate, fixed
+group_names = [bland]
+timesteps=len(indata)
+
 
 # derive needed cols + order indata
 I = indata["confirmed"]-indata["covered"]-indata["death"]
 I[I<0] = 0#sometimes there are more recovered than infected (?)
-S = pd.Series(N).repeat(len(indata)) - I.values - indata["covered"].values - indata["death"].values
-E = indata["death"]
-R = indata["covered"]
+I = I.values
+S = pd.Series(N).repeat(len(indata)).values - I - indata["covered"].values - indata["death"].values
+E = indata["death"].values
+R = indata["covered"].values
+y = np.stack([S,E,I,R],axis=1)
 
-datarray = 
+################
+###FIT
+################
+#TODO: Add R2-Calc
+# # fit model to data
+fit_pan = panfit.FitPandemicSimulator(beta=beta0, gamma=gamma, delta=delta, N=N, y=y,timesteps=timesteps,group_names=group_names)
+#result = fit_pan.fit_SEIR()
+#betaest = result.x[0]#[0] to drop np.array, otherwise __init__ does not turn it into vector
+fit_pan.fit_and_show_results()
+df = fit_pan.fit_and_extract_df()
+# fit_pan.simulate_SEIR()
+# fit_pan.simulate_and_show_results()
+#
 
-fit_pan = panfit.FitPandemicSimulator(beta=beta, gamma=gamma, delta=delta, N=N, y=dummy_y,timesteps=timesteps,group_names=group_names)
-result = fit_pan.fit_SEIR()
-result
-#Hakunama
 from importlib import reload
 reload(panfit)
-
+reload(pansingle)
 #dummy_y.flatten()
